deef07c54f452ea1a14b127144555777502d1bd4
Harshwardhan Mishra <HMishra@slb.com>
2022-05-17 14:44:29 +0530

    RevertChanges

--- DemoProject1.cpp
+++ DemoProject1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout<<"This is another push for Demo Project to learn the CI/CD Pipeline Implementation"<<endl;
+    cout<<"This is a Demo Project to learn the CI/CD Pipeline Implementation"<<endl;
 
     return 0;
 }
